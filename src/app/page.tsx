001bb62a829b6e20ab5bafa7fdb1530da8b0b5a1
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-06-20 20:24:03 +0200

    feat button in main page for learning useRouter hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,10 @@ export default function  Home(){
       Hello Next.js
       <div className="mt-14 p-2 bg-blue-400 w-48 text-center text-white rounded-2xl hover:bg-blue-700 transition-all duration-300 ">
       <Link  href="/dashboard"> go to dashboard </Link>
+      
       </div>
+
+      <button>move to dashboard</button>
       
     </div>
   )
