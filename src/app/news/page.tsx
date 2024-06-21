4de3ca8276ce06838dec1c925d0f12a08a6327a1
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-06-21 11:42:18 +0200

    feat: add news segment with its component and used map methode to show the list of news

---
+++ src/app/news/page.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+
+export default function NewsList(){
+    const news = [
+        {id:1,title:"news one" , content :"content one"},
+        {id:2,title:"news two" , content :"content two"},
+        {id:3,title:"news three" , content :"content three"},
+        {id:4,title:"news foure" , content :"content foure"}
+
+    ]
+
+    return(
+        <div>
+            <h1>News:</h1>
+            <ul>
+                {news.map((item)=>(
+                    <li key={item.id}>
+                       <a href={`/news/${item.id}`}>{item.title}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
