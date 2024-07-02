96f6cf8bb6a085d6a632c4b5824d1ea61d1c0f24
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-02 19:29:41 +0200

    feat: add style of not found component

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function NotFound() {
   return (
-    <div>
+    <div className='text-center text-red-700 font-extrabold'>
       <h1> Not found-404</h1>
       <p>There is not this page</p>
     </div>
