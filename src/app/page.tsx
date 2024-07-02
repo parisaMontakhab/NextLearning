c4fb43e1d21eb11c58491ff6fb9c055daa42f6a2
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-02 19:14:47 +0200

    refactor: changed the next.config.mjs for handeling customerror such as 404 / 500

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from 'next/navigation'
 
 export default function  Home(){
   
+  
   const router = useRouter()
   return(
     <div className="p-4 flex items-center justify-center ">
