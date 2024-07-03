f1e56761444956f3c83adefe3886912afff87404
parisaMontakhb <parisa.montakhabi@gmail.com>
2024-07-03 10:02:19 +0200

    feat: add new page for learning redirect

---
+++ src/app/new-page/page.tsx
@@ -0,0 +1,9 @@
+
+export default function NewPage(){
+    return(
+        <div className="text-center">
+            <h1 className="font-black">New Page</h1>
+            <p> This is a new page</p>
+        </div>
+    )
+}
